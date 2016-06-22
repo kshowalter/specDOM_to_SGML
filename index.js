d2eae44e2dbd42cafeb78eaaba462eff7ba4ca07
Keith Showalter <kshowalter@fsec.ucf.edu>
2016-06-22 10:38:46 -0400

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+* ConfigDOM constructor
+* @exports test
+* @constructor ConfigDOM
+* @param  {string} id id of the parent element
+* @return {object} ConfigDOM object
+*/
+module.exports = function(input){
+
+  return false;
+};
