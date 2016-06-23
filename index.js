ad17bd4468563012f34cc52d856259b997441cd1
Keith Showalter <kshowalter@fsec.ucf.edu>
2016-06-23 10:56:55 -0400

    Added basic functionality

--- index.js
+++ index.js
@@ -5,7 +5,38 @@
 * @param  {string} id id of the parent element
 * @return {object} ConfigDOM object
 */
-module.exports = function(input){
 
-  return false;
+var htmlize = function htmlize(specs){
+  if( specs.constructor === String ){
+    return specs;
+  }
+
+  var HTML_string = '<' + specs.tag;
+  if( specs.props ){
+    for( var name in specs.props ){
+      HTML_string += ' ' + name + '="' + specs.props[name] + '"';
+    }
+  }
+  HTML_string +=  '>';
+
+  if( specs.children ){
+    var childrenStringArray = specs.children.map(function(childSpec){
+      if( childSpec ){
+        return htmlize(childSpec);
+      }
+    });
+    HTML_string += childrenStringArray.join('');
+  }
+
+  HTML_string +=  '</' + specs.tag + '>';
+
+  return HTML_string;
+};
+
+
+
+module.exports = function(spec){
+  var HTML_string = htmlize(spec);
+
+  return HTML_string;
 };
